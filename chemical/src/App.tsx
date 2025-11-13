27b352a571e57b1ea3d377642e4ba48e4ae0459b
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2025-11-13 10:05:34 +0200

    Made interface and added API url

--- chemical/src/App.tsx
+++ chemical/src/App.tsx
@@ -2,6 +2,21 @@ import { useState, useEffect, useRef, useCallback } from 'react'
 import axios from 'axios'
 import './App.css'
 
+
+interface Material {
+  id: string;
+  type: string;
+  attributes: {
+    chemical_formula_reduced: string;
+    lattice_vectors?: number[][];
+    symmetry?: {
+      space_group_symbol?: string;
+    };
+  };
+}
+
+const API_URL = "https://optimade.materialsproject.org/v1/materials";
+
 function App() {
 
   return (
